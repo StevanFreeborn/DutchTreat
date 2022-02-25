cdede080f4686655e620b64e60e3c7d093414d44
StevanFreeborn <sfreeborn7@gmail.com>
2022-02-25 15:02:30 -0600

    Enabling MVC, adding first controller, adding first view.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var buyButton = $("#buyButton");
+    buyButton.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    // hide and show login form when login is clicked.
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function() {
+        $popupForm.fadeToggle(250);
+    });
+
+});
