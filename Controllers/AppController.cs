5826458f4a71e2c9d212c6586d12d264f85d122d
StevanFreeborn <sfreeborn7@gmail.com>
2022-02-26 11:34:29 -0600

    Began Implementing Contact Page

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -6,7 +6,28 @@ namespace DutchTreat.Controllers
     public class AppController : Controller
     {
         public IActionResult Index()
-        { 
+        {
+            ViewBag.Title = "Home Page";
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(object model)
+        {
+            return View();
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "About";
+
             return View();
         }
     }
