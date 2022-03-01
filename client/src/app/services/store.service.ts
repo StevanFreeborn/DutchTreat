117bc269fd8d680277e8e24d0fdc34fa933af5ab
StevanFreeborn <sfreeborn7@gmail.com>
2022-03-01 13:12:57 -0600

    Moved shop implementation from server-side to client-side; Began implementing cart;

---
+++ client/src/app/services/store.service.ts
@@ -0,0 +1,43 @@
+﻿import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Product } from "../shared/Product";
+import { Order, OrderItem } from "../shared/Order";
+
+@Injectable()
+export class Store
+{
+    constructor(private http: HttpClient)
+    {
+
+    }
+
+    public products: Product[] = [];
+
+    public order: Order = new Order();
+
+    loadProducts(): Observable<void>
+    {
+        return this.http.get<[]>("/api/products")
+            .pipe(map(data =>
+                    { 
+                        this.products = data;
+                        return;
+                    }));
+    }
+
+    addToOrder(product: Product)
+    {
+        const newItem = new OrderItem();
+        newItem.productId = product.id;
+        newItem.productTitle = product.title;
+        newItem.productArtId = product.artId;
+        newItem.productArtist = product.artist;
+        newItem.productCategory = product.category;
+        newItem.productSize = product.size;
+        newItem.quantity = 1;
+
+        this.order.items.push(newItem);
+    }
+}
---
+++ client/src/app/views/productListView.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from "@angular/core";
+import { OnInit } from "@angular/core";
+import { Store } from "../services/store.service";
+
+@Component(
+    {
+        selector: "product-list",
+        templateUrl: "productListView.component.html",
+        styleUrls: ["productListView.component.css"]
+    }
+)
+export default class ProductListView implements OnInit
+{
+    constructor(public store: Store)
+    {
+    }
+
+    ngOnInit(): void
+    {
+        this.store.loadProducts()
+            .subscribe(); // kicks off the operation
+    }
+}
